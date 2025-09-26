c76153d7310d4162d979eb4b23ec18285c4ba736
Amodh Dhakal <amodhakal@gmail.com>
2025-09-26 19:22:14 -0400

    Setup discover, portfolio, upload skeleton

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,11 @@
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+
+export default async function page() {
+  const { isAuthenticated } = await auth();
+  if (!isAuthenticated) {
+    return <RedirectToSignIn />;
+  }
+  
+  return <div className="">Upload</div>;
+}
