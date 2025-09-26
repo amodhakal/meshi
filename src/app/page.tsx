44df8dea3bb10d0a15042c6cb0aac6da6023c136
Amodh Dhakal <amodhakal@gmail.com>
2025-09-26 17:06:39 -0400

    Created a simple card for home dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import Card from "@/components/Card";
+import Container from "@/components/Container";
+import { data } from "@/constants/data";
+
 export default function page() {
+  const items = data;
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <p className="text-5xl font-black">Meshi</p>
+    <div className="w-screen min-h-screen">
+      <Container className="flex justify-center">
+        <div className="grid p-4 gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {items.map((data) => (
+            <Card key={data.id} data={data} />
+          ))}
+        </div>
+      </Container>
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,35 +5,39 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+
 import Link from "next/link";
+import Container from "./Container";
 
 export default function Header() {
   return (
     <>
-      <header className="flex justify-between items-center p-4 gap-4 shadow rounded-t-none rounded-xl">
-        <Link
-          href="/"
-          className="font-black text-xl text-rose-600 hover:text-rose-700"
-        >
-          MESHI
-        </Link>
-        <div className="flex justify-end gap-4">
-          <SignedOut>
-            <SignInButton>
-              <button className="bg-gray-100 hover:bg-gray-200 active:bg-gray-300 active:scale-95 text-gray-800 font-medium rounded-xl px-4 py-3 cursor-pointer">
-                Sign In
-              </button>
-            </SignInButton>
-            <SignUpButton>
-              <button className="bg-rose-600 hover:bg-rose-700 active:bg-rose-800 active:scale-95 text-white font-medium rounded-xl px-4 py-3 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </div>
+      <header className="w-screen shadow rounded-t-none rounded-xl">
+        <Container className="flex justify-between items-center p-4 gap-4">
+          <Link
+            href="/"
+            className="font-black text-xl text-rose-600 hover:text-rose-700"
+          >
+            MESHI
+          </Link>
+          <div className="flex justify-end gap-4">
+            <SignedOut>
+              <SignInButton>
+                <button className="bg-gray-100 hover:scale-105 active:bg-gray-300 active:scale-95 text-gray-800 font-medium rounded-xl px-4 py-3 cursor-pointer">
+                  Sign In
+                </button>
+              </SignInButton>
+              <SignUpButton>
+                <button className="bg-rose-600 hover:scale-105 active:bg-rose-800 active:scale-95 text-white font-medium rounded-xl px-4 py-3 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </Container>
       </header>
     </>
   );
