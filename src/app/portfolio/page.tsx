b00a7ec5c57e682a0b183b768428f5017c10c48a
Amodh Dhakal <amodhakal@gmail.com>
2025-09-28 22:27:14 -0400

    Implemented the portfolio cards

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,11 +1,26 @@
+import Container from "@/components/Container";
+import OwnedMeshi from "@/components/OwnedMeshi";
 import { RedirectToSignIn } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
+import { fetchQuery } from "convex/nextjs";
+import { api } from "../../../convex/_generated/api";
 
 export default async function page() {
-  const { isAuthenticated } = await auth();
-  if (!isAuthenticated) {
+  const { isAuthenticated, userId } = await auth();
+
+  if (!isAuthenticated || !userId) {
     return <RedirectToSignIn />;
   }
 
-  return <div className="">Portfolio</div>;
+  const ownedMeshis = await fetchQuery(api.query.getMeshiByCreatorId, {
+    creatorId: userId,
+  });
+
+  return (
+    <Container className="grid grid-cols-4 gap-4 pt-4">
+      {ownedMeshis.map((ownedMeshi) => (
+        <OwnedMeshi key={ownedMeshi._id} ownedMeshi={ownedMeshi} />
+      ))}
+    </Container>
+  );
 }
